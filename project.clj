c4ec4bf8dc275abccde66d89a718295c5e63ae4a
rhishikesh <rhishikesh@helpshift.com>
2020-12-20 21:17:56 +0530

    FIX : [Important] Change artifact id of the library.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mongrove "0.1.0-SNAPSHOT"
+(defproject helpshift/mongrove "0.1.0-SNAPSHOT"
   :description "An idiomatic Clojure wrapper for MongoDB java-driver 4.x"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
