85f4723d7a11a17bb1fcd344250b6b371e790331
rhishikesh <rhishikesh@helpshift.com>
2021-02-10 15:09:51 +0530

    Add signing info

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://medium.com/helpshift-engineering"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :signing {:gpg-key "helpshift-clojars-admin"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.mongodb/mongodb-driver-sync "4.1.1"]
                  [org.clojure/tools.logging "1.1.0"]]
